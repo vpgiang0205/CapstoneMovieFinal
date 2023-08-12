f73dc4da9d75466076152b72b45b3f59b7605503
Your Name <you@example.com>
2023-08-13 06:28:45 +0700

    update tab

--- src/pages/HomeTemplate/ListTheaterPage/TheaterLogo/index.js
+++ src/pages/HomeTemplate/ListTheaterPage/TheaterLogo/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
-export default function TheaterLogo() {
+export default function TheaterLogo(props) {
+  const {theater} = props
   return (
-    <div>
-      
-    </div>
+      <img src={theater.logo} />
   )
 }
--- src/pages/HomeTemplate/ListTheaterPage/index.js
+++ src/pages/HomeTemplate/ListTheaterPage/index.js
@@ -1,11 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import TheaterLogo from './TheaterLogo'
 import { useDispatch, useSelector } from 'react-redux'
 import { actTheater } from './duck/actions';
 
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+
 export default function ListTheaterPage() {
     const dispatch = useDispatch();
     const data = useSelector((state) => state.theaterReducer.data)
+
+    const [activeTab, setActiveTab] = useState(0);
 
     const loading = useSelector((state) => state.theaterReducer.loading)
 
@@ -22,18 +29,37 @@ export default function ListTheaterPage() {
         if (data) {
             return data.map((item) => {
                 console.log(item);
-                <div className='row bg-dark text-center'>
-                    <div className='col-2 bg-danger'> <img src= {item.logo} /></div>
-                    <div className='col-3 bg-warning'>ListTheaterAdress</div>
-                    <div className='col-7 bg-info'>ListTheaterMovie</div>
-                </div>
+                
+                return <Box display="flex">
+                <Tabs
+                    orientation="vertical"
+                    variant="scrollable"
+                    value={activeTab}
+                    onChange={handleTabChange}
+                >
+                    <Tab><img src = {item.logo} /></Tab>
+                 
+                </Tabs>
+                <Box p={3}>
+                    {activeTab === 0 && <div>Content for Tab 1</div>}
+                    {activeTab === 1 && <div>Content for Tab 2</div>}
+                    {activeTab === 2 && <div>Content for Tab 3</div>}
+                </Box>
+                <Box p={3}>
+                    {activeTab === 0 && <div>Content for Tab 1</div>}
+                    {activeTab === 1 && <div>Content for Tab 2</div>}
+                    {activeTab === 2 && <div>Content for Tab 3</div>}
+                </Box>
+            </Box>
             })
         }
     }
 
+    const handleTabChange = (event, newTab) => {
+        setActiveTab(newTab);
+    };
+
     return (
-        <div className='container'>
-            {renderTheater()}
-        </div>
+      <>{renderTheater()}</>
     )
 }
