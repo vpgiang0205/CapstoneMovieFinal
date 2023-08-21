4d1ec9ef630b0ebfe8eef8db404c28d27d97c9e2
WIN-3AUC1MTF8AI\Administrator <vophonggiang0205@gmail.com>
2023-08-21 18:35:45 +0700

    update design form login

--- src/pages/HomeTemplate/Login/index.js
+++ src/pages/HomeTemplate/Login/index.js
@@ -1,71 +1,67 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Navigate, useNavigate } from 'react-router-dom'
+import { NavLink, Navigate, useNavigate } from 'react-router-dom';
 import { actAuth } from 'redux/types/_actions';
 
 export default function Auth() {
-  const error = useSelector((state) => state.authReducer.error)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const error = useSelector((state) => state.authReducer.error);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [state, setState] = useState({
     taikhoan: "",
     matkhau: ""
   });
 
-
   const handleOnChange = (e) => {
     const { name, value } = e.target;
     setState({
       ...state,
       [name]: value,
-    })
-  }
+    });
+  };
 
   const handleLogin = (e) => {
     e.preventDefault();
-    dispatch(actAuth(state,navigate))
-  }
+    dispatch(actAuth(state, navigate));
+  };
 
   if (localStorage.getItem("USER_LOGIN")) {
-    return <Navigate replace to = "/" />
+    return <Navigate replace to="/" />;
   }
 
   const renderError = () => {
-
     return (
       error && (
-        <div className=' alert alert-danger'> {error?.response.data.content}</div>
+        <div className='alert alert-danger'> {error?.response.data.content}</div>
       )
-    )
-  }
+    );
+  };
 
   return (
-    <div className='container'>
-      <div className="form-group">
+    <div className='container d-flex justify-content-center align-items-center' style={{ minHeight: '100vh' }}>
+      <div className="form-group w-75">
 
-        <h1>Login</h1>
+        <h1 className="text-center mb-4">Đăng nhập</h1>
         {renderError()}
         <form onSubmit={handleLogin}>
           <div className='form-group'>
-            <label>Username:</label>
-            <input type="text" name="taikhoan" className="form-control" onChange={handleOnChange} />
-
+            <input type="text" name="taikhoan" className="form-control" placeholder='Tên đăng nhập' onChange={handleOnChange} />
           </div>
 
           <div className='form-group'>
-
-            <label>Username:</label>
-            <input type="text" name="matkhau" className="form-control" onChange={handleOnChange} />
+            <input type="password" name="matkhau" className="form-control" placeholder='Mật khẩu' onChange={handleOnChange} />
           </div>
 
           <div className='form-group'>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-success w-100">Login</button>
           </div>
 
+          <div className="mt-12 text-sm font-display font-semibold text-gray-700 text-center">
+            Bạn chưa có tài khoản ? <NavLink to="register" className="cursor-pointer text-indigo-600 hover:text-indigo-800">Đăng ký</NavLink>
+          </div>
         </form>
       </div>
-
-    </div >
-  )
+    </div>
+  );
 }
